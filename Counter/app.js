cd5bc10442bca9f9360e48f75b57664f480b5e47
nikolaypleshkov <icelol@abv.bg>
2021-05-17 17:41:03 +0300

    counter

---
+++ Counter/app.js
@@ -0,0 +1,24 @@
+const btns = document.querySelectorAll(".btn");
+const value = document.getElementById("value");
+let count = 0;
+
+btns.forEach(function(btn){
+    btn.addEventListener('click', function(e){
+        const styles = e.currentTarget.classList;
+
+        if(styles.contains('decrease')){
+            count --;
+        }
+        else if(styles.contains('increase')){
+            count++;
+        }
+        else{
+
+            count = 0;
+        }
+        value.textContent = count;
+
+    });
+});
+
+
