1f2163cb8593fef4b68bc6321e1230f06336505d
nikolaypleshkov <icelol@abv.bg>
2021-05-19 20:31:45 +0300

    Update App.tsx

--- react/my-app/src/App.tsx
+++ react/my-app/src/App.tsx
@@ -38,11 +38,32 @@ const App = () => {
     setLoading(false);
   }
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) =>{
+    if(!gameOver){
+      const answer = e.currentTarget.value;
 
+      const correct = questions[number].correct_answer === answer;
+
+      if(correct) setScore(prev => prev + 1);
+
+      const answerObject = {
+        question: questions[number].question,
+        answer,
+        correct,
+        correctAnswer: questions[number].correct_answer
+      };
+      setUserAnswers(prev => [...prev, answerObject])
+    }
   }
 
   const nextQuestion = () =>{
+    const nextQuestion = number + 1;
 
+    if(nextQuestion === TOTAL_QUESTIONS){
+      setGameOver(true);
+    }
+    else{
+      setNumber(nextQuestion);
+    }
   }
     return (
       <div className="App">
@@ -54,7 +75,7 @@ const App = () => {
         ): null}
        {!gameOver ?  <p className="score">Score:</p> : null}
         {loading && <p>Loading Questions</p>}
-        {!loading && !gameOver && (
+        {!loading && !gameOver &&(
          <QuizCard 
           questionNumber={number + 1}
           totalQuestion={TOTAL_QUESTIONS}
@@ -63,9 +84,10 @@ const App = () => {
           userAnswer={userAnswers ? userAnswers[number] : undefined}
           callback={checkAnswer}
         /> )}
+        {!gameOver && !loading && userAnswers.length === number + 1 && number !== TOTAL_QUESTIONS - 1 ? (
         <button className="next" onClick={nextQuestion}>
           NextQuestion
-        </button>
+        </button>): null}
         </div>
   );
 }
