9665ce2434e5a4edb6966f240aac749500565f12
nikolaypleshkov <icelol@abv.bg>
2021-05-19 13:45:35 +0300

    Update App.js

--- react/todo-list/src/App.js
+++ react/todo-list/src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState,useEffect } from 'react'; 
+import TodoForm from './components/todo-form';
+import TodoList from './components/todo-list';
 
+const LOCAL_STORAGE_KEY = 'react-todo-list-todos';
 function App() {
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    const storageTodos = localStorage.getItem(LOCAL_STORAGE_KEY);
+    if(storageTodos){
+      setTodos(storageTodos);
+    }
+  }, []);
+
+  useEffect(() => {
+   localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos]);
+
+  function addTodo(todo){
+    setTodos([todo, ...todos]);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Todo-List</p>
+        <TodoForm addTodo = {addTodo}  />
+        <TodoList todos = {todos} />
       </header>
     </div>
   );
