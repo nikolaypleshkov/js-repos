bd9c23044700a710e0921579222c48bac6183490
nikolaypleshkov <icelol@abv.bg>
2021-05-19 13:45:39 +0300

    Create todo-list.js

---
+++ react/todo-list/src/components/todo-list.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Todo from './todo';
+
+function TodoList({ todos }) {
+    return (
+        <ul>
+            {todos.map(todo => (
+                <Todo key = {todo.id} todo = {todo} />
+            ))}
+        </ul>
+    );
+}
+
+export default TodoList;
