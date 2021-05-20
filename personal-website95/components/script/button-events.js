d27b6e8bcfb432a690280f82fa1606d1397aa8e6
nikolaypleshkov <icelol@abv.bg>
2021-05-20 14:48:06 +0300

    Update button-events.js

--- personal-website95/components/script/button-events.js
+++ personal-website95/components/script/button-events.js
@@ -8,11 +8,6 @@ const dropDown = document.getElementById('dropdown-content');
 const aboutBtn = document.getElementById('aboutBtn');
 const aboutWin = document.querySelector('.about');
 
-const text_data = "Hi, my name is Nikolay Pleshkov. I'm a passionate Java and JavaScript developer. Right now I'm a student in Plovdiv University, major at B.SE. I have experiance building web application's frontend with React.js + Next.js and Angular" +
-" and the backend (server-side) with Java (Spring Boot, Spring Data JPA and Spring Security) + Hibernate. I have excellent knowledge of the OOP principles. Daily practising SOLID principles.I'm really into self-development. Looking forward for web/mobile developing.  "
-const text = document.querySelector('.card-text');
-
-text.textContent = text_data;
 
 
 startBtn.addEventListener('click', function(){
