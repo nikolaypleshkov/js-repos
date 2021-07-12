4b24ef07d1d86918a63af4f15d48bcd939e32910
nikolaypleshkov <icelol@abv.bg>
2021-07-12 15:35:47 +0300

    Create HeroSection

---
+++ quiz-game/src/components/HeroSection/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import TypeButton from '../Buttons'
+import Modal from '../Modal'
+
+function Hero() {
+    return (
+
+        <div className="wrapper">
+            <div className="container">
+                <div className="row align-items-center">
+                    <Modal />
+                </div>
+            </div>
+        </div>
+
+       
+
+
+
+    
+    )
+}
+
+export default Hero
