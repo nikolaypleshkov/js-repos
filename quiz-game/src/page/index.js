39beb232f513078ed581a4413e20d29a1664479b
nikolaypleshkov <icelol@abv.bg>
2021-07-12 15:36:21 +0300

    Create page folder

---
+++ quiz-game/src/page/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Hero from '../components/HeroSection'
+
+function HomePage() {
+    return (
+        <div class="d-flex justify-content-center">
+            <Hero />
+        </div>
+    )
+}
+
+export default HomePage
