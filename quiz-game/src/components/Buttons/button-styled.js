427f218c8a59f2cbe5de650a894bebf48848b824
nikolaypleshkov <icelol@abv.bg>
2021-07-12 15:35:31 +0300

    Creating Button Component

---
+++ quiz-game/src/components/Buttons/button-styled.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const Button = styled.button`
+  width: 110px;
+  height: 40px;
+  margin-top: 9%;
+  border-radius: 50px;
+  border: 0px;
+  background-color: #FF375F;
+  color: #fff;
+`
+
+export const Text = styled.p`
+  text-align: center;
+  display: inline;
+  font-family: 'Ubuntu', sans-serif;
+  font-weight: 600;
+  letter-spacing: 1.2px;
+`
---
+++ quiz-game/src/components/Buttons/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button, Text } from './button-styled'
+
+function TypeButton() {
+    return (
+        <Button >
+            <Text>Next</Text>
+        </Button>
+    )
+}
+
+export default TypeButton
