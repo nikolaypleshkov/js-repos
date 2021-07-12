e6dc0ccda69dad5818e9e0b573380306a5c78fc2
nikolaypleshkov <icelol@abv.bg>
2021-07-12 15:36:25 +0300

    Create data.js

---
+++ quiz-game/src/components/Quiz/data.js
@@ -0,0 +1,34 @@
+export const quizData = [
+    {
+        number: '1',
+        question: 'What is my name ?',
+        answers: {
+            a: 'Nikolay',
+            b: 'Stella',
+            c: 'Marko'
+        },
+        correct: 'a'
+    },
+
+    {
+        number: '2',
+        question: 'How old I am ?',
+        answers: {
+            a: '16',
+            b: '17',
+            c: '19'
+        },
+        correct: 'c'
+    },
+
+    {
+        number: '3',
+        question: 'Where am I from ?',
+        answers: {
+            a: 'Sozopol',
+            b: 'Burgas',
+            c: 'Plovdiv'
+        },
+        correct: 'a'
+    }
+];
