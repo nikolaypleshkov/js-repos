51c1d8881c242db07409735e2061d89ac1e0aee9
nikolaypleshkov <icelol@abv.bg>
2021-07-13 18:07:12 +0300

    Update App.js

--- voting-system/src/App.js
+++ voting-system/src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Voting from './components/Voting';
+import Main from './components/Main';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="voting">
+            <Voting />
+          </Route>
+          <Route exact path='/'>
+            <Main />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
