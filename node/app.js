8297c01f11be62d54d3e6087a16c7cf0334192bc
nikolaypleshkov <icelol@abv.bg>
2021-07-22 15:02:34 +0300

    Update app.js

--- node/app.js
+++ node/app.js
@@ -1,11 +1,9 @@
-const log = require('./logger');
 const path = require('path');
 const os = require('os');
 const fs = require('fs');
 const EventEmitter = require('events');
-const emitter = new EventEmitter();
 //log('opa')
-
+ 
 //Path Module
 var pathObj = path.parse(__filename);
 //OS Module
@@ -25,15 +23,20 @@ console.log(`Total Memory: ${freeMemory}`);
 const files = fs.readdirSync('./');
 
 console.log(files);
-
+ /*
 fs.readdir('./', function(reject, reslove) {
     if(reject) console.log('Error',reject);
     else console.log('Result',reslove);    
 });
-
-emitter.on('messageLogged', function() {
-    console.log('Listener called');
-})
+*/
 
 
-emitter.emit('messageLogged');
+const Logger = require('./logger');
+const logger = new Logger();
+
+//Register a listener
+logger.on('messageLogged', (e) => {
+    console.log('Listener called', e);
+});
+ 
+logger.log('message');
