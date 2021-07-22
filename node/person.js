aed60247e505f0f7d1be2c7d23c4895859ed39fe
nikolaypleshkov <icelol@abv.bg>
2021-07-22 15:32:06 +0300

    Create person.js

---
+++ node/person.js
@@ -0,0 +1,20 @@
+class Person{
+
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+
+    getName(){
+        console.log(this.name);
+    }
+    
+    getAge(){
+        console.log(this.age);
+    }
+
+}
+
+module.exports = Person;
+
+
