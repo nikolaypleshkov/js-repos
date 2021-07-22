3531350a7a03d4954789151faf8d8450eb7f898c
nikolaypleshkov <icelol@abv.bg>
2021-07-22 14:21:26 +0300

    os module

--- node/app.js
+++ node/app.js
@@ -1,11 +1,18 @@
 const log = require('./logger');
 const path = require('path');
-
+const os = require('os');
 //log('opa')
 
 //Path Module
 var pathObj = path.parse(__filename);
-
+var cpu = os.cpus();
+var totalMemory = os.totalmem();
+var freeMemory = os.freemem();
 console.log(pathObj);
-
+console.log('Information about each logical CPU core');
+console.log(cpu);
+console.log('Total Memory');
+console.log(totalMemory);
+console.log('Free Memory');
+console.log(freeMemory);
 
