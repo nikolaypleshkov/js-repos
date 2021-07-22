d2dbe2745936c5e9513da3698568d25abe4b35e1
nikolaypleshkov <icelol@abv.bg>
2021-07-22 14:32:53 +0300

    fs Module

--- node/app.js
+++ node/app.js
@@ -1,17 +1,31 @@
 const log = require('./logger');
 const path = require('path');
 const os = require('os');
+const fs = require('fs');
+
 //log('opa')
 
 //Path Module
 var pathObj = path.parse(__filename);
+//OS Module
 var cpu = os.cpus();
 var totalMemory = os.totalmem();
 var freeMemory = os.freemem();
+
 console.log(pathObj);
+
 console.log('Information about each logical CPU core');
 console.log(cpu);
 
 //ES6
 console.log(`Total Memory: ${totalMemory}`);
 console.log(`Total Memory: ${freeMemory}`);
+
+const files = fs.readdirSync('./');
+
+console.log(files);
+
+fs.readdir('$', function(reject, reslove) {
+    if(reject) console.log('Error',reject);
+    else console.log('Result',reslove);    
+});
